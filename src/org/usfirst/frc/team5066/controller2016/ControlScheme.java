607c8987ee25c8b3d3e8d2bdae31cbdcf5d38923
Michael Wolf <wolfhowl513@gmail.com>
2016-02-08 15:53:16 -0500

    Added the ControlScheme system to the index

---
+++ src/org/usfirst/frc/team5066/controller2016/ControlScheme.java
@@ -0,0 +1,9 @@
+package org.usfirst.frc.team5066.controller2016;
+
+public interface ControlScheme {
+	//contains all control methods that will be called in Robot.java
+	//such as moveElevator(), mecanumDrive(), etc.
+	
+	//implemented by all control scheme classes in the controlSchemes package, which take input from controller classes
+	//and are called by the ControlSystem class
+}
---
+++ src/org/usfirst/frc/team5066/controller2016/LogitechController.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team5066.controller2016;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+public class LogitechController extends Joystick{
+
+	public LogitechController(int port) {
+		super(port);
+	}
+	
+	//maps raw input methods to usefully named methods
+	
+}
