7a0c6069dbe52fb2b100aaf2e0458002c21ffbb6
Cameron Tressler <camtressler@gmail.com>
2016-02-22 18:32:31 -0500

    Again, VERY minor changes

--- src/org/usfirst/frc/team5066/robot/Robot.java
+++ src/org/usfirst/frc/team5066/robot/Robot.java
@@ -91,8 +91,7 @@ public class Robot extends IterativeRobot {
 			// Implement standard robotics things (input, drive, etc.). We will
 			// need to make this use the new controller classes later.
 			js = new Joystick(0);
-			drive = new SingularityDrive(frontLeftMotor, rearLeftMotor, frontRightMotor, rearRightMotor,
-					this.driveControllerType, slowSpeedConstant, normalSpeedConstant, fastSpeedConstant);
+			drive = new SingularityDrive(frontLeftMotor, rearLeftMotor, frontRightMotor, rearRightMotor, this.driveControllerType, slowSpeedConstant, normalSpeedConstant, fastSpeedConstant);
 			arm = new SingularityArm(2, 9, 7, 5, .25);
 			conveyer = new SingularityConveyer(8, 6);
 
